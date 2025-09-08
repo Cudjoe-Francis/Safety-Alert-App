d1394f35da5a116cd206ac904cbbb3ccca8ea0d8
Dennokim <dennokim@example.com>
2025-09-08 15:43:47 +0300

    Enhanced Android app to include user email in alert data for reply notifications

--- utils/sendAlert.ts
+++ utils/sendAlert.ts
@@ -5,5 +5,11 @@ import { AlertData } from "./buildAlertData";
 // Sends alert data to Firestore
 export async function sendAlertToFirestore(alert: AlertData) {
   // alert.serviceType should be set to "police", "fire", etc.
-  await addDoc(collection(firestore, "alerts"), alert);
+  // Ensure user email is included in the alert data
+  const alertWithEmail = {
+    ...alert,
+    userEmail: alert.user.email, // Add userEmail field for easy access
+  };
+  
+  await addDoc(collection(firestore, "alerts"), alertWithEmail);
 }
